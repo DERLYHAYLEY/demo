52697ceebdb90309837f724dbd0280bda430c2d2
DERLYHAYLEY <171676@unsaac.edu.pe>
2025-05-04 18:09:01 -0500

    creacion del index

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		/**hola mundo */
+		
 		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 
 }
